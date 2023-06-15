b7a5abe728ee5b8e4096eea34ef143152ddc8b7f
0xSavant <100378695+0xSavant@users.noreply.github.com>
2023-06-15 08:54:08 -0400

    change theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { Paper, Title, Center, Group, ActionIcon, Image } from '@mantine/core'
-import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
+import { Title, Center, Container, Image, Stack, Flex, Group, ActionIcon } from '@mantine/core'
 
 export default function Home() {
   return (
@@ -13,31 +12,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <Center p="sm">
-          <Title order={1} size={75}>Coming Soon...</Title>
+        <Center>
+          <Stack align="center">
+            <Image src="./logo.gif" alt="RugDude Logo" />
+            <Title order={1} size={52} color="green" sx={{ fontFamily: "Consolas" }}>rugg'd</Title>
+            <Title order={2} color="green" sx={{ fontFamily: "Consolas" }}>an nft experience by rugdude.</Title>
+          </Stack>
         </Center>
-        <Paper mt="sm" p="lg" radius="lg" shadow="lg">
-          <Image src="https://i.imgur.com/JzLgrRM.gif" alt="Stockpile x Rug Roulette Chip" width={350} height={350} />
-          <Group
-            spacing="xs"
-            position="center"
-            noWrap
-            sx={{
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-          >
-            <ActionIcon component="a" href="https://github.com/StockpileProtocol" target="_blank" size="xl" variant="default" radius="xl">
-              <FaGithub size={24} color="gray" />
-            </ActionIcon>
-            <ActionIcon component="a" href="https://twitter.com/0xgoonie" target="_blank" size="xl" variant="default" radius="xl">
-              <FaTwitter size={24} color="gray" />
-            </ActionIcon>
-            <ActionIcon component="a" href="https://discord.gg/mCN2gj2vgz" target="_blank" size="xl" variant="default" radius="xl">
-              <FaDiscord size={24} color="gray" />
-            </ActionIcon>
-          </Group>
-        </Paper>
+        <Group
+          spacing="sm"
+          position="center"
+          noWrap
+          sx={{
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <ActionIcon component="a" href="https://twitter.com/0xgoonie" size={75} variant="transparent">
+            <Image src="./twitter.jpg" />
+          </ActionIcon>
+          <ActionIcon component="a" size={75} variant="transparent">
+            <Image src="./discord.jpg" />
+          </ActionIcon>
+        </Group>
       </main>
     </>
   )
