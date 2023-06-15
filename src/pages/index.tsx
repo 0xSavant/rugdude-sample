4c221512c476616989352cc2a157206d73f8b9ee
0xSavant <100378695+0xSavant@users.noreply.github.com>
2023-06-15 10:06:28 -0400

    more updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Rug Roulette: Coming Soon</title>
-        <meta name="description" content="Rug Roulette: Coming Soon" />
+        <title>rugg'd: an experience by rugdude</title>
+        <meta name="description" content="web3 is about to get rugg'd." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -15,8 +15,31 @@ export default function Home() {
         <Center>
           <Stack align="center">
             <Image src="./logo.gif" alt="RugDude Logo" />
-            <Title order={1} size={52} color="green" sx={{ fontFamily: "Consolas" }}>rugg'd</Title>
-            <Title order={2} color="green" sx={{ fontFamily: "Consolas" }}>an nft experience by rugdude.</Title>
+            <Title
+              order={1}
+              size={52}
+              color="green"
+              sx={{
+                fontFamily: "Consolas"
+              }}
+            >
+              rugg'd
+            </Title>
+            <Title
+              order={2}
+              size={32}
+              align="center"
+              color="green"
+              sx={{
+                fontFamily: "Consolas",
+                "@media (max-width: 400px)": {
+                  alignSelf: "center",
+                  fontSize: 24,
+                },
+              }}
+            >
+              web3 is about to get rugg'd.
+            </Title>
           </Stack>
         </Center>
         <Group
@@ -29,10 +52,30 @@ export default function Home() {
             marginRight: "auto",
           }}
         >
-          <ActionIcon component="a" href="https://twitter.com/rugdudenft" size={75} variant="transparent">
+          <ActionIcon
+            component="a"
+            href="https://twitter.com/rugdudenft"
+            size={75}
+            variant="transparent"
+            sx={{
+              "@media (max-width: 400px)": {
+                blockSize: 25,
+              },
+            }}
+          >
             <Image src="./twitter.jpg" />
           </ActionIcon>
-          <ActionIcon component="a" size={75} variant="transparent">
+          <ActionIcon
+            component="a"
+            href="https://discord.gg/TbP7rhhvRd"
+            size={75}
+            variant="transparent"
+            sx={{
+              "@media (max-width: 400px)": {
+                blockSize: 25,
+              },
+            }}
+          >
             <Image src="./discord.jpg" />
           </ActionIcon>
         </Group>
