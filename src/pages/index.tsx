30c5337c03a64fbfdf94a3a527e91374bad27441
0xSavant <100378695+0xSavant@users.noreply.github.com>
2023-06-21 18:32:18 -0400

    i hope i fucking fixed this

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <div>
+        <div className={styles.logoWrapper}>
           <img
             className={styles.logoImg}
             src="./neonmain.gif"
